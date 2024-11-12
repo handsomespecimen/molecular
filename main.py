1695ff0d42e80185a47a8788b92db1499cd4fc40
handsomespecimen <gabsim0912@yahoo.com>
2024-11-12 17:50:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,17 @@ from flask import Flask, jsonify, request
 import chemicalfun as cf
 
 app = Flask(__name__)
-
 @app.route('/generate_reactions', methods=['POST'])
 def generate_reactions():
     formulas = request.json.get('formulas', [])
-    chemicalReactions = cf.ChemicalReactions(formulas)
-    reactions = chemicalReactions.generateReactions()
-    
-    reactions_dict = [{el[0]: el[1] for el in r} for r in reactions]
-    return jsonify(reactions_dict)
-
+    if not formulas:
+        return jsonify({"error": "no formulas specified"}), 400
+    try:
+        chemicaleeactions = cf.ChemicalReactions(formulas)
+        reactions = chemicalReactions.generateReactions()
+        reactions_dict = [{el[0]: el[1] for el in r} for r in reactions]
+        return jsonify(reactions_dict)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
