5660217a234e39772cfe11263de3b40be6e2523e
handsomespecimen <gabsim0912@yahoo.com>
2024-11-12 19:01:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -19,4 +19,4 @@ def generate_reactions():
         print(f"Error: {str(e)}")
         return jsonify({'error': 'An error occurred while processing your request'}), 500
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True)
