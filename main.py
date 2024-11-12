1cdbf83492182aded23ce203bb49373e8196d0c1
handsomespecimen <gabsim0912@yahoo.com>
2024-11-12 19:01:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,19 +3,20 @@ import os
 from flask import Flask, jsonify, request
 sys.path.append(os.path.join(os.path.dirname(__file__), 'chemicalfun'))
 import chemicalfun as cf
-
 app = Flask(__name__)
+
 @app.route('/generate_reactions', methods=['POST'])
 def generate_reactions():
-    formulas = request.json.get('formulas', [])
-    if not formulas:
-        return jsonify({"error": "no formulas specified"}), 400
     try:
-        chemicalreactions = cf.ChemicalReactions(formulas)
-        reactions = chemicalreactions.generateReactions()
-        reactions_dict = [{el[0]: el[1] for el in r} for r in reactions]
-        return jsonify(reactions_dict)
+        formulas = request.json.get('formulas')
+        if not formulas:
+            return jsonify({'error': 'No formulas provided'}), 400
+        chemicalReactions = cf.ChemicalReactions(formulas)
+        reactions = chemicalReactions.generateReactions()
+        reactions_list = [{'substance': el[0], 'coefficient': el[1]} for r in reactions for el in r]
+        return jsonify({'reactions': reactions_list})
     except Exception as e:
-        return jsonify({"error": str(e)}), 500
+        print(f"Error: {str(e)}")
+        return jsonify({'error': 'An error occurred while processing your request'}), 500
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5000)
